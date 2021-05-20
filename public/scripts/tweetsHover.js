8ff6e33c7828dfbfe6f884ecf78fbfba8ce9b184
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-20 06:49:43 +0000

    Create a new script file tweetsHover.js. Use document ready function. Target section class aswell as footer images. Use jQuery hover function for behaviour for mouseon and mouse off to set css.

---
+++ public/scripts/tweetsHover.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  // --- our code goes here ---
+
+  $("section.tweets").hover(function () {
+
+    $(this).css({"box-shadow" : "6px 6px grey"});
+
+  }, function () {
+
+    $(this).css({"box-shadow" : "0px 0px"});
+
+
+  });
+  $("footer.tweets i").hover(function () {
+
+    $(this).css({"color" : "orange"});
+
+  }, function () {
+
+    $(this).css({"color" : "#4056A1"});
+
+  });
+
+  // $(this).siblings(".buttonCount").children(".counter").css({"color" : "red"});
+
+
+
+
+
+
+
+});
