ea01a9b113857f94123dffab850b28424cf66627
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-20 23:08:48 +0000

    Add validation to jQuery form submit event

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,6 @@
  */
 $(document).ready(function () {
 
-  // Fake data taken from initial-tweets.json
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   function createTweetElement(tweetData) {
     return $(`
@@ -74,24 +48,28 @@ $(document).ready(function () {
       url: "/tweets",
       method: "GET",
     }).then((result) => {
-      console.log("test", result);
     renderTweets(result);
     })
   };
 
 loadData();
-//renderTweets(data);
+
 
 
   $("form").submit(function (event) {
     event.preventDefault()
-
-    $.ajax({
-      url: "/tweets",
-      method: "POST",
-      data: $(this).children("textarea").serialize(),
-      success: console.log('yes'),
-    });
+    
+    if($(this).children("textarea").val().length > 140) {
+      alert("Message exceeds character limit!");
+    } else if($(this).children("textarea").val() === "") {
+      alert("Message is empty!")
+    } else {
+     $.ajax({
+       url: "/tweets",
+       method: "POST",
+       data: $(this).children("textarea").serialize(),
+     });
+    }
 
   });
 
