0d52309cec5150a7ea469c1be664b7af682cbdcd
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-20 08:51:56 +0000

    Create Dynamic Tweets. In client.js create new function createNewTweets that returns html template string of tweets. Create new function renderTweets thats recieves an array of tweets data objects. The function loops through each element and calls the createNewTweets function. The returns html string is then appended to the main container

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,7 +5,103 @@
  */
 $(document).ready(function() {
 
-console.log($("footer.tweets span").text(timeago.format(1621491959543)));
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+function createTweetElement(tweetData){
+return $(`
+<section class="tweets">
+        <article>
+          <header class="tweets">
+            <div>
+              <img src="${tweetData.user.avatars}">
+              <span>${tweetData.user.name}</span>
+            </div>
+              <span>${tweetData.user.handle}</span>
+            
+          </header>
+          <p>
+            ${tweetData.content.text}
+          </p>
+          <footer class="tweets">
+            <span>${timeago.format(tweetData.created_at)}</span>
+            <div>
+              <i class="fas fa-flag"></i>
+              <i class="fas fa-retweet"></i>
+              <i class="fas fa-heart"></i>
+            </div>
+          </footer>
+        </article>
+      </section>
+
+`)};
+
+
+function renderTweets(tweetsData){
+
+tweetsData.forEach(element => {
+  let $tweet = createTweetElement(element);
+  $('main.container').append($tweet);
+});
+
+}
+
+renderTweets(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 });
