bd91dec4bd6954a3129c0b4ae6c19ce4d9340a6d
Pavneet Kang <pavneet.kang2@gmail.com>
2021-05-21 05:05:11 +0000

    Clean code and fix formatting

--- public/scripts/tweetsHover.js
+++ public/scripts/tweetsHover.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
   // --- our code goes here ---
-//dynamic hover css for tweet box
+ //dynamic hover css for tweet box
   $("main.container").on('mouseover', 'section.tweets', function () {
     $(this).css({ "box-shadow": "6px 6px grey" });
   });
   $("main.container").on('mouseout', 'section.tweets', function () {
     $(this).css({ "box-shadow": "0px 0px" });
   });
-//dynamic hover css for footer links
+
+ //dynamic hover css for footer links
   $("main.container").on('mouseover', "footer.tweets i", function () {
     $(this).css({ "color": "orange" });
   });
